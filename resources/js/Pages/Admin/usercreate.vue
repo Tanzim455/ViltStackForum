859f651b977169feb83e0dbd685aa3dfc391e4a0
Tanzim455 <tanzim.ibthesam@gmail.com>
2023-12-22 23:10:58 +0600

    Views added

---
+++ resources/js/Pages/Admin/usercreate.vue
@@ -0,0 +1,93 @@
+<script setup>
+import BreezeButton from '@/Components/Button.vue';
+
+import BreezeInput from '@/Components/Input.vue';
+import BreezeLabel from '@/Components/Label.vue';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+//  import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import { useForm,Link } from '@inertiajs/vue3'
+
+
+
+import Header from './header.vue';
+import SuccessMessage from '@/Components/SuccessMessage.vue';
+const form = useForm({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+
+    role_id:'',
+    terms:false
+});
+defineProps({
+    'roles':{
+        type:Object
+    }
+})
+
+const submit = () => {
+    form.post(route('admin.usersstore'), {
+        onFinish: () => form.reset('password', 'password_confirmation'),
+    });
+    form='';
+};
+</script>
+
+<template>
+<Header />
+<SuccessMessage />
+<div class="container mx-auto mt-32">
+
+
+        <BreezeValidationErrors class="mb-4" />
+
+        <form @submit.prevent="submit">
+            <div>
+                <BreezeLabel for="name" value="Name" />
+                <BreezeInput id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+            </div>
+
+            <div class="mt-4">
+                <BreezeLabel for="email" />
+                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autocomplete="username" />
+            </div>
+            <div class="mt-4">
+                <select
+                class="bg-gray-50 border border-gray-300
+                text-gray-900 text-sm rounded-lg focus:ring-blue-500
+                focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
+                dark:border-gray-600 dark:placeholder-gray-400 dark:text-whit
+                e dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model="form.role_id"
+
+                >
+                <option selected>Choose role</option>
+                <option  v-for="role in roles"
+                :key="role.id" :value="role.id">{{role.name}}</option>
+               ></select>
+            </div>
+            <div class="mt-4">
+                <BreezeLabel for="password" value="Password" />
+                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <BreezeLabel for="password_confirmation" value="Confirm Password" />
+                <BreezeInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                    Already registered?
+                </Link>
+
+                <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Register
+                </BreezeButton>
+            </div>
+        </form>
+
+</div>
+
+</template>
---
+++ resources/js/Pages/User/authuserinfo.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <label>Change your name</label><br>
+    <form @submit.prevent="handleSubmit">
+    Your name-{{user.name}}<br>
+<input type="text" v-model="form.name" class="px-6 py-1 border rounded-md mb-2" /><br>
+<button class="bg-blue-600 text-white px-6 py-1 text-white font-serif">Save</button>
+    </form>
+
+
+</div>
+
+</template>
+
+<script setup>
+import { useForm, usePage } from '@inertiajs/inertia-vue3';
+import { computed } from '@vue/reactivity';
+
+const user = computed(() => usePage().props.value.auth.user)
+let form=useForm({
+    id:user.id,
+    name:user.name
+})
+const handleSubmit=()=>{
+
+    form.post(route('profile.update'));
+
+}
+
+</script>
